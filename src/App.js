8d12262174dafc5a2a1bcdb9443c1980a68084f4
dominik-billensteiner <56152933+dominik-billensteiner@users.noreply.github.com>
2020-05-15 13:31:30 +0200

    Add dynamic color to background, depending on temperature.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
-import WeatherApp from "./WeatherApp";
+import WeatherApp from "./components/WeatherApp";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <WeatherApp />
+      <WeatherApp temp="24" />
+      <WeatherApp temp="-10" />
+      <WeatherApp temp="40" />
     </div>
   );
 }
---
+++ src/components/WeatherApp.js
@@ -0,0 +1,81 @@
+import React from "react";
+const highTempMax = 40;
+const highTempMin = 25;
+//const midTempMax = 24;
+//const midTempMin = 10;
+const lowTempMax = 9;
+const lowTempMin = -20;
+
+// sfc = stateless functional component
+const WeatherApp = (props) => {
+  let highColor = 0;
+  let lowColor = 0;
+  let tempBg = null;
+
+  if (props.temp >= 12) {
+    // "high" redish background
+    highColor =
+      (1 - (props.temp - highTempMin) / (highTempMax - highTempMin)) * 255;
+    lowColor = highColor / 150;
+    // Prepare background style
+    tempBg = {
+      background: `linear-gradient(
+        to top, 
+        rgb(255, ${highColor}, 0),
+        rgb(255, ${lowColor}, 0)
+        )`,
+    };
+  } /*else if (props.temp < 25 && props.temp > 10) {
+    // "middle" orangeish background
+    highColor =
+      (1 - (props.temp - midTempMin) / (midTempMax - midTempMin)) * 180;
+    lowColor = 138;
+    // Prepare background style
+    tempBg = {
+      background: `linear-gradient(
+        to top, 
+        rgb(255, ${highColor}, 0),
+        rgb(255, ${lowColor}, 0)
+        )`,
+    };*/ else if (
+    props.temp < 12
+  ) {
+    // "low" blueish background
+    highColor =
+      (1 - (props.temp - lowTempMin) / (lowTempMax - lowTempMin)) * 255;
+    lowColor = highColor / 150;
+    // Prepare background style
+    tempBg = {
+      background: `linear-gradient(
+        to top, 
+        rgb(0, ${highColor}, 255),
+        rgb(0, ${lowColor}, 255)
+        )`,
+    };
+  }
+
+  return (
+    <div className="card" style={tempBg}>
+      <div className="card__location">
+        <h1 className="location__city">Vienna</h1>
+        <h3 className="location__country">AT</h3>
+      </div>
+      <img className="icon" src="./img/cloudy.png" alt="Weather Icon" />
+      <h1 className="temperature">12 °C</h1>
+      <h3 className="condition">Cloudy</h3>
+    </div>
+  );
+};
+
+export default WeatherApp;
+
+/* Hot weater temp
+ * tMin = 25°C; tMax = 40°C
+ * Red: RGBA(R=255, G=Variable, B=0)
+ * 25°C => G 0
+ * 40°C => G 40
+ *
+ */
+
+// Medium weather temp = 10 - 20 °C: orange
+// Cold weater temp <= 10 °C: blue
