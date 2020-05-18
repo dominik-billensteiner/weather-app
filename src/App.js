e42e6b4d37629fae0c947329e23c74d2f1da2711
dominik-billensteiner <56152933+dominik-billensteiner@users.noreply.github.com>
2020-05-18 15:20:01 +0200

    Implement useEffect hook to load default values on component mount. Error Handling failed.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import WeatherCard from "./components/WeatherCard";
 import "./App.css";
 
 function App() {
   // State variable for input of city
-  const [cityInput, setCityInput] = useState("");
+  const [cityInput, setCityInput] = useState("Linz,at");
 
   // State variables for all displayed weather components
   const [weather, setWeather] = useState({
@@ -16,14 +16,18 @@ function App() {
 
   // Prepare data fetching from OpenWeather API
   const data = async (query) => {
-    return (
-      // Weather data of queried location in metric units
-      (
-        await fetch(
-          `http://api.openweathermap.org/data/2.5/weather?q=${query}&units=metric&APPID=7361f9a0de8a8b42fbc6b557cc6c0fa1`
-        )
-      ).json()
-    );
+    try {
+      return (
+        // Weather data of queried location in metric units
+        (
+          await fetch(
+            `http://api.openweathermap.org/data/2.5/weather?q=${query}&units=metric&APPID=7361f9a0de8a8b42fbc6b557cc6c0fa1`
+          )
+        ).json()
+      );
+    } catch (e) {
+      console.error(e);
+    }
   };
 
   // Search function
@@ -32,6 +36,22 @@ function App() {
     e.preventDefault();
     // Refresh data, when promise is resolved
     data(cityInput).then((res) => {
+      console.log(res);
+      if (!res.ok && res.cod !== "404")
+        setWeather({
+          city: res.name,
+          country: res.sys.country,
+          temperature: res.main.temp,
+          condition: res.weather[0].main,
+        });
+      // else throw new Error(res.status);
+      else console.error(res.status);
+    });
+  };
+
+  // Query default city when componenent is mounted
+  useEffect(() => {
+    data(cityInput).then((res) => {
       setWeather({
         city: res.name,
         country: res.sys.country,
@@ -39,8 +59,9 @@ function App() {
         condition: res.weather[0].main,
       });
     });
-  };
+  }, []);
 
+  // Return weather information as WeatherCard
   return (
     <div className="App">
       <WeatherCard
